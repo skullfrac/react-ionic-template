48a8cc972af1eb79f30433f66c18f5c4eff3e8cb
general <general@general.local>
2019-10-27 08:12:54 +0100

    added react component/container pattern and according basic layout

--- frontend/resources/App.tsx
+++ frontend/resources/App.tsx
@@ -1,30 +1,16 @@
-import React, { Component } from 'react';
-import '@ionic/core/css/core.css';
-import '@ionic/core/css/ionic.bundle.css';
-import {
-  IonApp,
-  IonContent,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardSubtitle
-} from '@ionic/react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <IonApp>
-        <IonContent>
-          <IonCard>
-            <IonCardHeader>
-              <IonCardSubtitle>Welcome to Ionic</IonCardSubtitle>
-              <IonCardTitle>Running on React</IonCardTitle>
-            </IonCardHeader>
-          </IonCard>
-        </IonContent>
-      </IonApp>
-    );
-  }
-}
+import { IonApp } from '@ionic/react';
 
-export default App;
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+import Home from '/srv/app/frontend/src/page/Home';
+
+const App = () => (
+  <IonApp>
+    <Home />
+  </IonApp>
+);
+
+export default App
